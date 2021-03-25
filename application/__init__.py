e34181cd97855f28da3ff27e4198d3536caaad53
semyonsh <github@semyon.eu>
2021-03-25 13:06:30 +0100

    cleanup

--- application/__init__.py
+++ application/__init__.py
@@ -5,11 +5,6 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.TestingConfig")
 
-    Bootstrap(app)
-
-    db.init_app(app)
-    
-
     with app.app_context():
         from . import routes  # Import routes
         
